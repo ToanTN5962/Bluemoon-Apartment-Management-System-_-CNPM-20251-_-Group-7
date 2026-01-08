71fca51f48e32eecd97d4cf8f3b406ce1b1d12ed
ToanTN <tnt728595@gmail.com>
2026-01-08 23:36:52 +0700

    Fix bug show information in account page

--- BluemoonApartmentManagementSystem/server/src/middleware/auth.js
+++ BluemoonApartmentManagementSystem/server/src/middleware/auth.js
@@ -1,17 +1,18 @@
 const jwt = require("jsonwebtoken");
 
-function authMiddleware(req, res, next) {
+const authMiddleware = (req, res, next) => {
   const authHeader = req.headers.authorization;
-  if (!authHeader) return res.status(401).json({ error: "No token provided" });
+  if (!authHeader) return res.status(401).json({ message: "No token provided" });
 
-  const token = authHeader.split(" ")[1];
+  const token = authHeader.split(' ')[1];
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.userId = decoded.id;
+    req.userId = decoded.userId;
+    req.userEmail = decoded.email;
+    req.userRole = decoded.role;
     next();
   } catch (err) {
     return res.status(401).json({ error: "Invalid token" });
   }
 }
-
 module.exports = authMiddleware;
--- BluemoonApartmentManagementSystem/server/src/routes/route.js
+++ BluemoonApartmentManagementSystem/server/src/routes/route.js
@@ -1,12 +1,13 @@
 function route(app){
     console.log("Initializing routes...");
-    console.log(process.env.JWT_SECRECT);
+    console.log(process.env.JWT_SECRET);
     app.use("/api/auth", require("./auth.route"));
     app.use("/api/users", require("./user.route"));
     app.use("/api/residents", require("./resident.route"));
     app.use("/api/households", require("./household.route"));
     app.use("/api/fees", require("./fee.route"));
-    app.use("/api/complaints", require("./complaint.route"));
+    // app.use("/api/complaints", require("./complaint.route"));
+    // console.log("test");
 
     //app.use("/api/bills", require("./bill.route"));
     //app.use("/api/payments", require("./payment.route"));
